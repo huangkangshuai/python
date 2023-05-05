de365207e6f9c5b39b090d5be71e0bd65dbd4c67
LENOVOK14GEN1\86185 <1134712291@qq.com>
2023-05-05 16:24:27 +0800

    20230505更新文件完整性嗅探器之识别数据项缺失

--- python/python_demo/完整性嗅探器之识别数据项缺失_训练模型.py
+++ python/python_demo/完整性嗅探器之识别数据项缺失_训练模型.py
@@ -1,7 +1,28 @@
+# 读取CSV文件，并对数据进行去噪处理，即去除数据中的异常值。
+#
+# 对枚举字段进行处理，将枚举类转换为数字。
+#
+# 对非枚举字段进行向量化处理，将空值转换为0，非空值转换为1。
+#
+# 对每个有空值的字段训练一个MLP分类器，并存储在models字典中。
+#
+# 针对每个有空值的字段，预测其空值，用MLP分类器进行预测。
+#
+# 进行主成分分析，将数据降维。
+#
+# 计算每个主成分对总方差的贡献比例。
+#
+# 将处理后的数据保存到CSV文件中。
+#
+# 在训练流程中，主要使用了sklearn库中的MLPClassifier和PCA两个模块。同时还使用了pandas和numpy库对数据进行处理和计算。在训练过程中，针对有空值的字段进行了处理，使用了MLP分类器进行预测填充空值。最后，对数据进行主成分分析，以提高模型效率和精度。
 import pandas as pd
 import numpy as np
+import pickle
 from sklearn.decomposition import PCA
 from sklearn.neural_network import MLPClassifier
+import joblib
+
+
 np.seterr(divide='ignore',invalid='ignore')
 data = pd.read_csv('C:/Users/86185/Desktop/datatest.csv')
 
@@ -23,8 +44,9 @@ for col in data.columns:
     else:
         data[col] = data[col].apply(lambda x: 0 if pd.isnull(x) else 1)
 
-# 训练模型
-models = {}
+# 训练模型并将其存储在models字典中
+models = joblib.load('C:/Users/86185/Desktop/models.pkl')
+
 for col in data.columns:
     if col != 'label' and data[col].isnull().sum() > 0:
         clf = MLPClassifier(random_state=42, max_iter=1000)
@@ -51,3 +73,6 @@ if total_var == 0:
 else:
     explained_variance_ratio_ = np.nan_to_num(explained_variance_ / total_var)
 data.to_csv('C:/Users/86185/Desktop/processed_data.csv', index=False)
+
+# 将models存储
+joblib.dump(models, 'C:/Users/86185/Desktop/models.pkl')
