4b84fcc8bb9c17e5e7942afe9eb00f6a74d03cdc
LENOVOK14GEN1\86185 <1134712291@qq.com>
2023-05-25 14:36:32 +0800

    20230525更新文件

--- python/python_demo/完整性嗅探器之识别数据项缺失_训练模型.py
+++ python/python_demo/完整性嗅探器之识别数据项缺失_训练模型.py
@@ -1,76 +1,118 @@
-# 读取CSV文件，并对数据进行去噪处理，即去除数据中的异常值。
-# 对枚举字段进行处理，将枚举类转换为数字。
-# 对非枚举字段进行向量化处理，将空值转换为0，非空值转换为1。
-# 对每个有空值的字段训练一个MLP分类器，并存储在models字典中。
-# 针对每个有空值的字段，预测其空值，用MLP分类器进行预测。
-# 进行主成分分析，将数据降维。
-# 计算每个主成分对总方差的贡献比例。
-# 将处理后的数据保存到CSV文件中。
-# 在训练流程中，主要使用了sklearn库中的MLPClassifier和PCA两个模块。同时还使用了pandas和numpy库对数据进行处理和计算。在训练过程中，针对有空值的字段进行了处理，使用了MLP分类器进行预测填充空值。最后，对数据进行主成分分析，以提高模型效率和精度。
 import pandas as pd
 import numpy as np
-from sklearn.decomposition import PCA
-from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
 import joblib
-from sklearn.cluster import KMeans
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
-np.seterr(divide='ignore',invalid='ignore')
-data = pd.read_csv('C:/Users/86185/Desktop/datatest.csv', encoding='gbk')
+# 读取数据集
+np.seterr(divide='ignore', invalid='ignore')
+data = pd.read_csv('C:/Users/86185/Desktop/数据集/空值异常数据集/空值正常训练数据.csv', encoding='gbk')
 
 # 去除噪音
 numeric_cols = data.select_dtypes(include='number').columns
 Q1 = data[numeric_cols].quantile(0.25)
 Q3 = data[numeric_cols].quantile(0.75)
 IQR = Q3 - Q1
-data = data[~((data[numeric_cols] < (Q1 - 1.5 * IQR)) | (data[numeric_cols] > (Q3 + 1.5 * IQR))).any(axis=1)]
+threshold = 3  # 调整离群值的阈值
+data = data[~((data[numeric_cols] < (Q1 - threshold * IQR)) | (data[numeric_cols] > (Q3 + 1.5 * IQR))).any(axis=1)]
 
-# 枚举字段处理 向量化处理
+# 向量化处理
 for col in data.columns:
-    if col.startswith('enum_'):
-        unique_values = data[col].unique()
-        if len(unique_values) < 10:
-            # 少于 10 种分类
-            enum_map = {value: idx + 1 for idx, value in enumerate(unique_values)}
-            data[col] = data[col].map(enum_map).fillna(0).astype(int)
-        else:
-            # 大于等于 10 种分类
-            data[col] = data[col].fillna(0).astype(int)
+    # 处理枚举字段
+    if data[col].dtype == 'object':
+        group_counts = data.groupby(col)[col].nunique()
     else:
-        # 非枚举字段
+        group_counts = data.groupby(col).nunique()
+
+    if len(group_counts) < 10:
+        enum_map = {np.nan: 0}  # 空值对应0
+        enum_values = group_counts.index.values
+        enum_classes = min(len(enum_values), 9)  # 最多映射为9个不同的值
+        for i, value in enumerate(enum_values[:enum_classes]):
+            enum_map[value] = i + 1  # 其他枚举类依次编码为1-9
+        data[col] = data[col].map(enum_map).fillna(0).astype(int)
+    else:
         data[col] = data[col].notna().astype(int)
 
-# 训练模型并将其存储在models字典中
-models = joblib.load('C:/Users/86185/Desktop/models.pkl')
-# models = {}
-for col in data.columns:
-    if col != 'label' and data[col].isnull().sum() > 0:
-        if col in models:
-            clf = models[col]
-        else:
-            clf = MLPClassifier(random_state=42, max_iter=1000)
-        clf.fit(data.dropna(subset=[col]).drop('label', axis=1), data.dropna(subset=[col])['label'])
-        models[col] = clf
+# 创建空字典用于存储模型
+models = {}
 
-# 预测空值
-for col in models.keys():
-    null_idx = data[data[col].isnull()].index
-    if len(null_idx) > 0:
-        preds = models[col].predict(data.loc[null_idx, :].drop('label', axis=1))
-        data.loc[null_idx, col] = preds
+result_table = pd.DataFrame()
 
-# 主成分分析
-pca = PCA(n_components=0.9)
-pca.fit(data.drop('label', axis=1))
-components = pca.transform(data.drop('label', axis=1))
+for ii in data.columns:
+    # 划分特征和目标变量
+    X = data.drop(ii, axis=1).values
+    y = data[ii]
 
-# 计算主成分分析（PCA）的解释方差比例
-explained_variance_ = pca.explained_variance_
-total_var = np.sum(explained_variance_)
-if total_var == 0:
-    explained_variance_ratio_ = np.zeros(len(pca.explained_variance_ratio_))
-else:
-    explained_variance_ratio_ = np.nan_to_num(explained_variance_ / total_var)
-data.to_csv('C:/Users/86185/Desktop/processed_data.csv', index=False)
+    # 创建训练集和测试集
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# 将models存储
-joblib.dump(models, 'C:/Users/86185/Desktop/models.pkl')
+    # 训练模型
+    model = RandomForestClassifier(random_state=42)
+    model.fit(X_train, y_train)
+
+    # 特征重要性排序
+    importance_scores = model.feature_importances_
+    feature_importance = pd.DataFrame({'Feature': data.drop(ii, axis=1).columns, 'Importance': importance_scores})
+    feature_importance = feature_importance.sort_values(by='Importance', ascending=False)
+
+    # 选择前N个重要特征
+    num_features = 5  # 选择前5个重要特征
+    selected_features = feature_importance.head(num_features)['Feature'].values
+
+    # 重新划分特征和目标变量
+    X = data[selected_features].values
+
+    # 创建训练集和测试集
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+    # 训练模型
+    model = RandomForestClassifier(random_state=42)
+    model.fit(X_train, y_train)
+
+    # 评估模型
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f'Target Field: {ii}, Accuracy: {accuracy}')
+
+    # 如果准确率大于0，则将模型保存到本地
+    if accuracy > 0:
+        models[ii] = model
+        joblib.dump(model, f"C:/Users/86185/Desktop/数据集/空值异常数据集/{ii}_perfect_model.joblib")
+
+        # 加载模型
+        model = joblib.load(f'C:/Users/86185/Desktop/数据集/空值异常数据集/{ii}_perfect_model.joblib')
+
+        # 读取数据集
+        np.seterr(divide='ignore', invalid='ignore')
+        dataX = pd.read_csv('C:/Users/86185/Desktop/数据集/空值异常数据集/空值异常测试数据.csv', encoding='gbk')
+        # 向量化处理
+        for col in dataX.columns:
+            # 处理枚举字段
+            if dataX[col].dtype == 'object':
+                group_counts = dataX.groupby(col)[col].nunique()
+            else:
+                group_counts = dataX.groupby(col).nunique()
+
+            if len(group_counts) < 10:
+                enum_map = {np.nan: 0}  # 空值对应0
+                enum_values = group_counts.index.values
+                enum_classes = min(len(enum_values), 9)  # 最多映射为9个不同的值
+                for i, value in enumerate(enum_values[:enum_classes]):
+                    enum_map[value] = i + 1  # 其他枚举类依次编码为1-9
+                dataX[col] = dataX[col].map(enum_map).fillna(0).astype(int)
+            else:
+                dataX[col] = dataX[col].notna().astype(int)
+
+        # 提取特征列
+        X = dataX[selected_features].values
+
+        # 进行预测
+        predictions = model.predict(X)
+
+        # 将预测结果添加到测试数据集中
+        df_predictions = pd.DataFrame({f'{ii}': predictions})
+        result_table = pd.concat([result_table, df_predictions], axis=1)
+result_table.to_csv(f'C:/Users/86185/Desktop/数据集/空值异常数据集/predictions.csv', index=False)
+print(f'预测结果输出predictions.csv')
